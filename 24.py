b9f51370024966593f203a4a5ef32549114ab94c
tzheng846 <96885766+tzheng846@users.noreply.github.com>
2024-08-26 02:27:39 -0700

    two card algo complete, three card work in progress

--- 24.py
+++ 24.py
@@ -1,11 +1,20 @@
 import random
+from itertools import combinations
 
+example_card = [12, 2, 1, 6]
+
+'''
+generates random 4 numbers from 1 to 13
+'''
 def generate_cards():
     all_cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
     cards =[]
     for i in range(4):
         cards.append(int(random.choice(all_cards)))
     return cards
+'''
+displays numbers as cards- 11 is J, 12 is Q, 13 is K
+'''
 def show_cards(cards):
     dct = {11:"J",12:"Q",13:"K"}
     for i in range(len(cards)):
@@ -13,15 +22,47 @@ def show_cards(cards):
             cards[i] = dct[cards[i]]
     return cards
 
-def apply_augment(num1,num2):
-    return [num1+num2,num1-num2,num2-num1,num1*num2,num1/num2,num2/num1,]
+'''
+applies basic functions to list of 4 numbers and saves the 6 results
+indexs:
+0 - add
+1 - subtract a-b
+2 - subtract b-a
+3 - multiply
+4 - divide a/b
+5 - divide b/a
+'''
+def two_card_algo(cards= example_card[:2]):
+    results= [cards[0]+cards[1],cards[0]-cards[1],cards[1]-cards[0],cards[0]*cards[1],cards[0]/cards[1],cards[1]/cards[0]]
+    return results
+def check_solution(results,cards):
+    solution = []
+    if(results[0]==24):
+        solution.append("{}{}{}{}".format(cards[0]," + ",cards[1]," = 24"))
+    if(results[1]==24):
+        solution.append("{}{}{}{}".format(cards[0]," - ",cards[1]," = 24"))
+    if(results[2]==24):
+            solution.append("{}{}{}{}".format(cards[1]," - ",cards[0]," = 24"))
+    if(results[3]==24):
+            solution.append("{}{}{}{}".format(cards[0]," * ",cards[1]," = 24"))
+    if(results[4]==24):
+            solution.append("{}{}{}{}".format(cards[0]," / ",cards[1]," = 24"))
+    if(results[5]==24):
+            solution.append("{}{}{}{}".format(cards[1]," / ",cards[0]," = 24"))
 
-def find_combo():
-    #DSF
-    cards = generate_cards()
-    for i in range(1,4):
-        new_cards = apply_augment(cards[0],cards[i])
-        print(cards)
-        print(new_cards)
+    if(not solution):
+        print("No Solution for " + str([cards[0],cards[1]]))
+    return solution
 
-find_combo()
+def three_card_algo(cards=example_card[:3]):
+    CARD_COMBINATIONS = [(0, 1),(0, 2),(1, 2)]
+
+    solutions = []
+    for i in CARD_COMBINATIONS:
+        two_card = two_card_algo([cards[i[0]],cards[i[1]]])
+        third_card_index = [x for x in (0,1,2) if x not in i][0]
+        for i in two_card:
+            three_card = two_card_algo([i,cards[third_card_index]])
+            print(three_card)
+        
+three_card_algo()
